bbcc73f567ae9804259b7c6b4f7c831e3be460d4
Zach Ohara <zjohara@gmail.com>
2015-08-24 02:45:10 -0700

    Add basic teleop program

---
+++ src/io/github/thunderbots/teleop/TeleOp.java
@@ -0,0 +1,47 @@
+/* Copyright (C) 2015 Thunderbots-5604
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.thunderbots.teleop;
+
+import io.github.thunderbots.sdk.Robot;
+import io.github.thunderbots.sdk.control.TGamepad;
+import io.github.thunderbots.sdk.drive.DriveMotorSet;
+import io.github.thunderbots.sdk.drive.DriveSystem;
+import io.github.thunderbots.sdk.drive.TankDrive;
+import io.github.thunderbots.sdk.opmode.TLinearOpMode;
+
+public class TeleOp extends TLinearOpMode {
+	
+	private DriveSystem drive;
+	private TGamepad drivingGamepad;
+	
+	private static final String[] DRIVE_MOTOR_NAMES = {"front_left", "front_right", "back_left", "back_right"};
+
+	@Override
+	protected void initializeRobot() {
+		DriveMotorSet motorSet = new DriveMotorSet(DRIVE_MOTOR_NAMES);
+		this.drive = new TankDrive(motorSet);
+		this.drivingGamepad = Robot.getGamepad1();
+	}
+
+	@Override
+	protected void main() {
+		while (this.opModeIsActive()) {
+			this.drive.setMovement(drivingGamepad.leftStickY(), drivingGamepad.leftStickX());
+		}
+	}
+
+}
