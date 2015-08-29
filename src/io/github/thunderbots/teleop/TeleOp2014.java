51469b6a1dc9f932aaa92c6b533961c26d3bc9ad
Pranav Mathur <pranavmathur99@gmail.com>
2015-08-29 11:08:57 -0700

    Add TeleOp for 2014 robot

--- src/io/github/thunderbots/teleop/TeleOp2014.java
+++ src/io/github/thunderbots/teleop/TeleOp2014.java
@@ -18,15 +18,20 @@ package io.github.thunderbots.teleop;
 
 import io.github.thunderbots.sdk.Robot;
 import io.github.thunderbots.sdk.control.TGamepad;
+import io.github.thunderbots.sdk.hardware.TMotor;
 import io.github.thunderbots.sdk.drive.DriveMotorSet;
 import io.github.thunderbots.sdk.drive.DriveSystem;
 import io.github.thunderbots.sdk.drive.TankDrive;
 import io.github.thunderbots.sdk.opmode.TLinearOpMode;
 
-public class TeleOp extends TLinearOpMode {
+public class TeleOp2014 extends TLinearOpMode {
 	
 	private DriveSystem drive;
 	private TGamepad drivingGamepad;
+	private TGamepad secondaryGamepad;
+	
+	private TMotor conveyor;
+	private TMotor lift;
 	
 	private static final String[] DRIVE_MOTOR_NAMES = {"front_left", "front_right", "back_left", "back_right"};
 
@@ -35,13 +40,25 @@ public class TeleOp extends TLinearOpMode {
 		DriveMotorSet motorSet = new DriveMotorSet(DRIVE_MOTOR_NAMES);
 		this.drive = new TankDrive(motorSet);
 		this.drivingGamepad = Robot.getGamepad1();
+		this.secondaryGamepad = Robot.getGamepad2();
+		this.conveyor = Robot.getMotor("conveyor");
 	}
 
 	@Override
 	protected void main() {
 		while (this.opModeIsActive()) {
 			this.drive.setMovement(drivingGamepad.leftStickY(), drivingGamepad.leftStickX());
+			this.setConveyorJoystick();
 		}
+	}
+	
+	private void setConveyorJoystick() {
+		if (this.secondaryGamepad.rightButton())
+			conveyor.setPower(1.0);
+		else if (this.secondaryGamepad.rightBumper())
+			conveyor.setPower(-1.0);
+		else
+			conveyor.setPower(0);
 	}
 
 }
