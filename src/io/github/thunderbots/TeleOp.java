601efefb0c91ac9738ca0e4ced65bc2050c1ee48
Zach Ohara <zjohara@gmail.com>
2015-08-31 23:58:58 -0700

    Move TeleOp to main package

--- src/io/github/thunderbots/TeleOp.java
+++ src/io/github/thunderbots/TeleOp.java
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package io.github.thunderbots.teleop;
+package io.github.thunderbots;
 
 import io.github.thunderbots.sdk.Robot;
 import io.github.thunderbots.sdk.control.TGamepad;
