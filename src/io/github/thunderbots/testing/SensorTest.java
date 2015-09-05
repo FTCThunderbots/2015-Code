bc8ab1b789ab9d46fb519e0d352fec29d2d688e0
Zach Ohara <zjohara@gmail.com>
2015-09-04 21:43:08 -0700

    Add touch sensor test

---
+++ src/io/github/thunderbots/testing/SensorTest.java
@@ -0,0 +1,44 @@
+package io.github.thunderbots.testing;
+
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import io.github.thunderbots.sdk.TRobot;
+import io.github.thunderbots.sdk.opmode.SimpleOpMode;
+
+
+/**
+ * 
+ *
+ * @author Zach Ohara
+ */
+public class SensorTest extends SimpleOpMode {
+	
+	private TouchSensor touch;
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected String[] getDriveMotorNames() {
+		return new String[0];
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void initializeRobot() {
+		super.initializeRobot();
+		this.touch = TRobot.getTouchSensor("touchsensor");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void main() {
+		while (this.opModeIsActive()) {
+			TRobot.sendTelemetryData("pressed", this.touch.isPressed());
+			TRobot.sendTelemetryData("value", this.touch.getValue());
+		}
+	}
+	
+}
