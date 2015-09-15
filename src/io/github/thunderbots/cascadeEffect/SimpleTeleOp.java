b50fb8edaefde5acada4d04fc698e9037ced02e6
Zach Ohara <zjohara@gmail.com>
2015-09-14 23:43:10 -0700

    Add a SimpleTeleOp for the cascade effect robot

---
+++ src/io/github/thunderbots/cascadeEffect/SimpleTeleOp.java
@@ -0,0 +1,36 @@
+/* Copyright (C) 2015 Thunderbots-5604
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.thunderbots.cascadeEffect;
+
+import io.github.thunderbots.annotation.OpMode;
+import io.github.thunderbots.lightning.opmode.TeleOp;
+
+@OpMode(type="TeleOp", name="CESimple")
+public class SimpleTeleOp extends TeleOp {
+
+	private static final String[] DRIVE_MOTOR_NAMES =
+			{"front_left", "front_right", "back_left", "back_right"};
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected String[] getDriveMotorNames() {
+		return SimpleTeleOp.DRIVE_MOTOR_NAMES;
+	}
+
+}
