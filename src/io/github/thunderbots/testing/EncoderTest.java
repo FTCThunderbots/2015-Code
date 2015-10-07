39bd3ebcf748ed366e360c16a24a559f3e6deb7a
Pranav Mathur <pranavmathur99@gmail.com>
2015-10-06 21:36:30 -0700

    Add Encoder Test

---
+++ src/io/github/thunderbots/testing/EncoderTest.java
@@ -0,0 +1,44 @@
+package io.github.thunderbots.testing;
+
+import io.github.thunderbots.lightning.Lightning;
+import io.github.thunderbots.lightning.hardware.Motor;
+import io.github.thunderbots.lightning.opmode.SimpleOpMode;
+
+public class EncoderTest extends SimpleOpMode {
+	
+	private Motor testMotor;
+	
+	@Override
+	protected void initializeRobot() {
+		this.testMotor = Lightning.getMotor("test");
+	}
+
+	@Override
+	protected String[] getDriveMotorNames() {
+		return null;
+	}
+
+	@Override
+	protected void main() {
+		testMotor.setPower(.5);
+		long currentTime = System.currentTimeMillis();
+		while (System.currentTimeMillis() < currentTime + 5000) {
+			Lightning.sendTelemetryData("Encoder", testMotor.getRawPosition());
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		testMotor.stop();
+		Lightning.sendTelemetryData("Encoder", testMotor.getRawPosition());
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
