b71a3c6067529e86602348d831b1de36d5aa2fc0
Pranav Mathur <pranavmathur99@gmail.com>
2015-10-09 14:31:49 -0700

    Add Util.sleep() to EncoderTest

--- src/io/github/thunderbots/testing/EncoderTest.java
+++ src/io/github/thunderbots/testing/EncoderTest.java
@@ -3,6 +3,7 @@ package io.github.thunderbots.testing;
 import io.github.thunderbots.lightning.Lightning;
 import io.github.thunderbots.lightning.hardware.Motor;
 import io.github.thunderbots.lightning.opmode.LightningOpMode;
+import io.github.thunderbots.lightning.utility.Util;
 
 public class EncoderTest extends LightningOpMode {
 	
@@ -15,24 +16,10 @@ public class EncoderTest extends LightningOpMode {
 	@Override
 	protected void main() {
 		testMotor.setPower(.5);
-		long currentTime = System.currentTimeMillis();
-		while (System.currentTimeMillis() < currentTime + 5000) {
-			Lightning.sendTelemetryData("Encoder", testMotor.getRawPosition());
-			try {
-				Thread.sleep(50);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+		Util.sleep(5000);
 		testMotor.stop();
 		Lightning.sendTelemetryData("Encoder", testMotor.getRawPosition());
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Util.sleep(5000);
 	}
 
 }
