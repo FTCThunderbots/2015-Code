607c8001a0a3b3155df7f17857e65d72cd8823a4
Zach Ohara <zjohara@gmail.com>
2015-10-21 14:42:39 -0700

    Add a test program for the task scheduling system

---
+++ src/io/github/thunderbots/testing/TaskSchedulerTest.java
@@ -0,0 +1,37 @@
+package io.github.thunderbots.testing;
+
+import io.github.thunderbots.lightning.Lightning;
+import io.github.thunderbots.lightning.opmode.LightningOpMode;
+
+public class TaskSchedulerTest extends LightningOpMode {
+
+	@Override
+	protected void initializeRobot() {
+		Lightning.getTaskScheduler().registerTask(new TestRunnable("A"));
+		Lightning.getTaskScheduler().registerTask(new TestRunnable("B"));
+		Lightning.getTaskScheduler().registerTask(new TestRunnable("C"));
+	}
+
+	@Override
+	protected void main() {
+		while (this.opModeIsActive()) {
+			// repeat
+		}
+	}
+	
+	public static class TestRunnable implements Runnable {
+		
+		private String s;
+		
+		public TestRunnable(String s) {
+			this.s = s;
+		}
+		
+		@Override
+		public void run() {
+			Lightning.sendTelemetryData(s);
+		}
+		
+	}
+
+}
