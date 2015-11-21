71e33a64181a845ede3f94941ae39fb3d840d366
Jake Ohara <jakeohara12@gmail.com>
2015-11-21 09:48:29 -0800

    Remove AutoMountainRed

--- src/io/github/thunderbots/testing/EncoderTest.java
+++ src/io/github/thunderbots/testing/EncoderTest.java
@@ -17,12 +17,15 @@
 package io.github.thunderbots.testing;
 
 import io.github.thunderbots.lightning.Lightning;
+import io.github.thunderbots.lightning.annotation.Active;
 import io.github.thunderbots.lightning.hardware.Motor;
 import io.github.thunderbots.lightning.opmode.LightningOpMode;
 import io.github.thunderbots.lightning.utility.Util;
 
-public class EncoderTest extends LightningOpMode {
+@Active
 
+public class EncoderTest extends LightningOpMode {
+	
 	private Motor testMotor;
 	private EncoderUpdateRunnable encoderRunnable;
 	
