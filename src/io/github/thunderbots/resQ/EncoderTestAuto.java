878b66834d8b1d072ee2ed88d67e6fb9d0a8ec7a
Zach Ohara <zjohara@gmail.com>
2015-12-09 19:51:45 -0800

    Fix EncoderTestAuto

--- src/io/github/thunderbots/resQ/EncoderTestAuto.java
+++ src/io/github/thunderbots/resQ/EncoderTestAuto.java
@@ -3,6 +3,7 @@ package io.github.thunderbots.resQ;
 import io.github.thunderbots.lightning.annotation.Active;
 import io.github.thunderbots.lightning.annotation.OpMode;
 import io.github.thunderbots.lightning.opmode.Autonomous;
+import io.github.thunderbots.lightning.utility.Util;
 
 @OpMode(name="EncoderTestAuto", type="Autonomous")
 @Active
@@ -26,10 +27,11 @@ public class EncoderTestAuto extends Autonomous{
 	
 	@Override
 	protected void main() {
-		this.getRobot().getDrive().driveTicks(EncoderTestAuto.DRIVE_POWER, 500);
-		this.getRobot().getDrive().rotateTicks(EncoderTestAuto.TURN_POWER, 500);
-		this.getRobot().getDrive().swingTicks(false, EncoderTestAuto.SWING_POWER, 500);
-		
+		this.getRobot().getDrive().driveTicks(EncoderTestAuto.DRIVE_POWER, 3500);
+		Util.sleep(2000);
+		this.getRobot().getDrive().rotateTicks(EncoderTestAuto.TURN_POWER, 3500);
+		Util.sleep(2000);
+		this.getRobot().getDrive().swingTicks(false, EncoderTestAuto.SWING_POWER, 3500);
 	}
 
 }
