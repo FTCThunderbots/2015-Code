bcea1bbaecbfaa1b8420b5302d050dd2af0692cb
Pranav Mathur <pranavmathur99@gmail.com>
2015-12-10 16:20:36 -0800

    Test autonomous encoders
    The robot succeeded in the following test:
    Drive forward 24 inches
    Rotate 180 degrees
    Drive forward 24 inches

--- src/io/github/thunderbots/resQ/ResQAutoEncoders.java
+++ src/io/github/thunderbots/resQ/ResQAutoEncoders.java
@@ -1,6 +1,7 @@
 package io.github.thunderbots.resQ;
 
 import io.github.thunderbots.lightning.opmode.Autonomous;
+import io.github.thunderbots.lightning.utility.Util;
 
 public abstract class ResQAutoEncoders extends Autonomous {
 	
@@ -22,7 +23,11 @@ public abstract class ResQAutoEncoders extends Autonomous {
 
 	@Override
 	protected void main() {
-		this.getRobot().getDrive().driveInches(.5, 12);
+		this.getRobot().getDrive().driveInches(.5, 24);
+		Util.sleep(3000);
+		this.getRobot().getDrive().rotateDegrees(.5, 180);
+		Util.sleep(3000);
+		this.getRobot().getDrive().driveInches(.5, 24);
 	}
 
 }
