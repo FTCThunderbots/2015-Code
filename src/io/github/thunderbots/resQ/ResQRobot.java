3f97742f2a26e6ba1dcb86f57b4a9cb7dca8f5a5
Jake Ohara <jakeohara12@gmail.com>
2015-12-11 18:53:09 -0800

    Fix servo allignment issues

--- src/io/github/thunderbots/resQ/ResQRobot.java
+++ src/io/github/thunderbots/resQ/ResQRobot.java
@@ -133,7 +133,7 @@ public class ResQRobot extends Robot {
 	}
 	
 	public void moveBucket(int pos) {
-		if (pos == -1)
+		if (pos == 1)
 			this.bucketTilt.moveToPosition(0);
 		else if (pos == 0)
 			this.bucketTilt.moveToPosition(0.5);
