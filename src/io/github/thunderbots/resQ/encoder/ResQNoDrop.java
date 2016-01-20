c6dc73ddf063d6727820db73c0417e03eb8039e2
Pranav Mathur <pranavmathur99@gmail.com>
2016-01-19 22:11:14 -0800

    Add new no-drop autonomous
    OpMode only drops the skydivers and then stops

---
+++ src/io/github/thunderbots/resQ/encoder/ResQNoDrop.java
@@ -0,0 +1,56 @@
+package io.github.thunderbots.resQ.encoder;
+
+import io.github.thunderbots.annotation.Active;
+import io.github.thunderbots.annotation.OpMode;
+import io.github.thunderbots.lightning.drive.DriveSystem;
+import io.github.thunderbots.lightning.opmode.Autonomous;
+import io.github.thunderbots.lightning.utility.Util;
+import io.github.thunderbots.resQ.ResQRobot;
+
+/**
+ * @author Jake Ohara
+ */
+@OpMode(type="Autonomous", name="Auto No Sky [E]")
+@Active
+public class ResQNoDrop extends Autonomous {
+	
+	private static final double MOVEMENT_POWER = 0.8;
+	private static final long STEP_DELAY = 1500; // in milliseconds
+	
+	@Override
+	protected ResQRobot getRobot() {
+		return (ResQRobot) super.getRobot();
+	}
+	
+	@Override
+	protected void initializeOpMode() {
+		super.initializeOpMode();
+		this.setRobot(new ResQRobot());
+	}
+
+	@Override
+	protected void main() {
+		DriveSystem ds = this.getRobot().getDrive(); // for brevity
+		ResQRobot resQRobot = this.getRobot(); //creating instance of the class here
+		
+		// start in front of the driver station, should be lined up in a straight line
+		// directly to the beacon repair area
+		// drive forward to the beacon repair area
+		
+		/*
+		 * Drives forward using MOVEMENT_POWER for some inches up until the beacon zone, it then stops for 1500 milliseconds.
+		 */
+		ds.driveInches(MOVEMENT_POWER, 92);
+		Util.sleep(STEP_DELAY);
+		
+		Util.sleep(STEP_DELAY);
+		
+		/*
+		 * Moves the Climber Arm back to default position and sleeps for 500 milliseconds.
+		 */
+
+		Util.sleep(500);
+
+	}
+	
+}	
