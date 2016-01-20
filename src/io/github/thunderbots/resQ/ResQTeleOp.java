753fc41268bc190530d8e8a1abda1eadb7f301c1
Pranav Mathur <pranavmathur99@gmail.com>
2016-01-19 21:54:53 -0800

    Move blockers to vertical buttons

--- src/io/github/thunderbots/resQ/ResQTeleOp.java
+++ src/io/github/thunderbots/resQ/ResQTeleOp.java
@@ -102,10 +102,10 @@ public class ResQTeleOp extends TeleOp {
 			this.getRobot().moveArmExtensor(0);
 		}
 		
-		if (Lightning.getJoystick(2).leftBumper()) {
+		if (Lightning.getJoystick(2).downButton()) {
 			this.getRobot().openLeftBlocker();
 			this.getRobot().openRightBlocker();
-		} else if (Lightning.getJoystick(2).rightBumper()) {
+		} else if (Lightning.getJoystick(2).upButton()) {
 			this.getRobot().closeLeftBlocker();
 			this.getRobot().closeRightBlocker();
 		}
