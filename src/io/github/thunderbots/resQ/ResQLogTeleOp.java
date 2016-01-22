a8a9366c8e06b73468f9350b1997823a7d894a06
Pranav Mathur <pranavmathur99@gmail.com>
2016-01-21 22:58:26 -0800

    Add Log TeleOp

--- src/io/github/thunderbots/resQ/ResQLogTeleOp.java
+++ src/io/github/thunderbots/resQ/ResQLogTeleOp.java
@@ -2,11 +2,15 @@ package io.github.thunderbots.resQ;
 
 import io.github.thunderbots.annotation.Active;
 import io.github.thunderbots.annotation.OpMode;
+import io.github.thunderbots.lightning.control.layout.ControlLayout;
+import io.github.thunderbots.lightning.control.layout.LogControlLayout;
 
 @OpMode(name = "Log TeleOp", type = "TeleOp")
 @Active
 public class ResQLogTeleOp extends ResQTeleOp {
 	
-	
+	public ControlLayout createControlLayout() {
+		return new LogControlLayout();
+	}
 	
 }
